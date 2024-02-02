ab0a4deacebd500a2b5559c0d26acfa86fbad22a
Anelize Nardelli <nardellianelize20@gmail.com>
2024-02-02 15:28:48 -0300

    DataBase criada e CRUD feito.

---
+++ EFExemplo/EFExemplo/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace EFExemplo.Models
+{
+    public class Cliente
+    {
+        public Guid ClienteId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateOnly Data { get; set; }
+
+    }
+}
